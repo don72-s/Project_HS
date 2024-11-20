116aca8ae2192cd8566d009589cbdca9a8deb5fb
김민태 <66211881+don72-s@users.noreply.github.com>
2024-11-20 10:49:20 +0900

    작업 공간 준비
    작업공간 분리/씬 분할 작업 진행.

---
+++ Assets/ProjectHS/Scenes/GameScene/GameSceneTester.cs
@@ -0,0 +1,57 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneTester : MonoBehaviourPunCallbacks {
+
+    public const string RoomName = "TestRoom";
+
+    private void Start() {
+
+        PhotonNetwork.LocalPlayer.NickName = $"Player {Random.Range(1000, 10000)}";
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+
+    public override void OnConnectedToMaster() {
+
+        Debug.Log("접속됨");
+
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 8;
+
+        PhotonNetwork.JoinOrCreateRoom(RoomName, options, TypedLobby.Default);
+
+    }
+
+    public override void OnJoinedRoom() {
+
+        Debug.Log("방에 들어와졌따!");
+        StartCoroutine(WaitCo());
+    }
+
+    IEnumerator WaitCo() {
+
+        yield return new WaitForSeconds(1);
+        TestGameStart();
+
+    }
+
+    public void TestGameStart() {
+
+        Debug.Log("게임이 시작했따;");
+
+        SpawnPlayer();
+
+    }
+
+    void SpawnPlayer() {
+
+        Debug.Log("플레이어를 스폰한다!");
+
+    }
+
+}
+
